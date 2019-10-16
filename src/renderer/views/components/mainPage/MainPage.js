f832d87432433b696a3c8910a60e990444d982ce
fengjutian <2640111081@qq.com>
2019-10-16 18:04:38 +0800

    save

---
+++ src/renderer/views/components/mainPage/MainPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Mainpage.scss';
+
+function App() {
+  return (
+    <div className="main-page">
+      <section className='page-refurbish'>
+        <button>刷新</button>
+      </section>
+      <section className='img-all-box'>
+        <div className='single-img-box'>
+
+
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
