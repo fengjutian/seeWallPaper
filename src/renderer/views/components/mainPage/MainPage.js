d1f2c2b6491e6871b60fe4a18dcb855b4b07d000
berg.feng <fengjutian>
2019-10-16 20:49:28 +0800

    save

--- src/renderer/views/components/mainPage/MainPage.js
+++ src/renderer/views/components/mainPage/MainPage.js
@@ -1,20 +1,118 @@
 import React from 'react';
 import './Mainpage.scss';
 
-function App() {
-  return (
-    <div className="main-page">
-      <section className='page-refurbish'>
-        <button>刷新</button>
-      </section>
-      <section className='img-all-box'>
-        <div className='single-img-box'>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imgList: [
+        {
+          url: require("../../assets/i_1.jpg"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_2.jpg"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_3.jpg"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_4.png"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_5.jpg"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_1.jpg"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_2.jpg"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_3.jpg"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_4.png"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_5.jpg"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_1.jpg"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_2.jpg"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_3.jpg"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_4.png"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_5.jpg"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_1.jpg"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_2.jpg"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_3.jpg"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_4.png"),
+          title: 'xxx'
+        },
+        {
+          url: require("../../assets/i_5.jpg"),
+          title: 'xxx'
+        }
+      ]
+    }
+  }
+  render(){
+    return (
+      <div className="main-page">
+        <section className='page-refurbish'>
+          <button>刷新</button>
+        </section>
+        <section className='img-all-box'>
+          {
+            this.state.imgList.map((item, key) => {
+              return (
+                <div className='single-img-box'>
+                  <div className='single-box-head'>
+                    <img src={item.url} key={key} alt=""/>
+                  </div>
+                  <div className='single-box-bottom'></div>
+                </div>
+              )
+            })
+          }
+        </section>
+      </div>
+    )
+  }
 
-
-        </div>
-      </section>
-    </div>
-  );
 }
 
 export default App;
